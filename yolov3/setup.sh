4649892dc4472c104e6f5273579afd727e1cad0b
dchung117 <dchung0330@gmail.com>
2022-11-25 20:37:03 +0000

    made setup script for data.

---
+++ yolov3/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+bash Anaconda3-2022.10-Linux-x86_64.sh
+source .bashrc
+
+# Install kaggle
+pip install kaggle
+
